99e2aedeab30e7cc166a1b6fc22bd9247a2895d7
Alex Sanford <alex.sanford1@gmail.com>
2012-07-10 09:31:54 -0300

    Implented before_filter storage

--- lib/locomotive/plugin.rb
+++ lib/locomotive/plugin.rb
@@ -1,5 +1,25 @@
 
 module Locomotive
-  class Plugin
+  module Plugin
+
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    module ClassMethods
+      def before_filter(meth)
+        @@before_filters ||= []
+        @@before_filters << meth
+      end
+
+      def before_filters
+        @@before_filters
+      end
+    end
+
+    def before_filters
+      self.class.before_filters
+    end
+
   end
 end
--- spec/lib/locomotive/plugin_spec.rb
+++ spec/lib/locomotive/plugin_spec.rb
@@ -4,9 +4,34 @@ require 'spec_helper'
 module Locomotive
   describe Plugin do
 
-    it 'should store a list of before_filters'
+    before(:each) do
+      @plugin = MyPlugin.new
+    end
+
+    it 'should store a list of before_filters' do
+      @plugin.before_filters.count.should == 2
+      @plugin.before_filters[0].should == :my_method1
+      @plugin.before_filters[1].should == :my_method2
+    end
 
     it 'should store a list of liquid drops'
+
+    protected
+
+    class MyPlugin
+      include Locomotive::Plugin
+
+      before_filter :my_method1
+      before_filter :my_method2
+
+      def my_method1
+        'This is my first before filter!'
+      end
+
+      def my_method1
+        'This is my second before filter!'
+      end
+    end
 
   end
 end
